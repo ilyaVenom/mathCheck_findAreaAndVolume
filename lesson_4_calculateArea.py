9ad2b43ccb1e4764c2b1f3f49d7378bf3e406137
ilya segal <65099651+ilyaVenom@users.noreply.github.com>
2024-01-29 21:41:13 -0500

    Add files via upload

---
+++ lesson_4_calculateArea.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 19 23:13:19 2023
+@author: segal
+"""
+#practicing python again from Bro Code
+# at 5 pm to 10 pm daily starting  
+# clip 1 out of 106
+# second ex. calculate the area of a rectangle:
+length = float(input("Enter the length of a rectangle: "))
+width = float(input("Enter the width of a rectangle: "))
+height = float(input("Enter the height of a rectangle: "))
+
+area = length * width
+
+volume = length * width * height
+
+print(f"The area is: {area}cm^2")
+print(f"The volume is: {volume}cm^3")
